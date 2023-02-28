4005a9504983f2e53828b154986db4a42f1e0631
Owen Voke <owen.voke@worksome.com>
2023-02-28 09:27:42 +0000

    chore(deps): update Lighthouse to 6.x

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Worksome\PestGraphqlCoverage;
 
+use GraphQL\Language\AST\FieldDefinitionNode;
 use GraphQL\Language\AST\InterfaceTypeDefinitionNode;
 use GraphQL\Language\AST\NodeKind;
 use GraphQL\Language\Parser;
@@ -180,8 +181,11 @@ class Plugin implements AddsOutput, HandlesArguments
         $schema = Parser::parse($output);
 
         $nodes = [];
+
         Visitor::visit($schema->definitions, [
-            NodeKind::FIELD_DEFINITION => function ($node, $key, $parent, $path, $ancestors) use (&$nodes) {
+            NodeKind::FIELD_DEFINITION => function (FieldDefinitionNode $node, $key, $parent, $path, $ancestors) use (
+                &$nodes
+            ) {
                 $parentType = $ancestors[1];
 
                 if ($parentType instanceof InterfaceTypeDefinitionNode) {
@@ -191,6 +195,7 @@ class Plugin implements AddsOutput, HandlesArguments
                 $nodes[$parentType->name->value][$node->name->value] = true;
             }
         ]);
+
         return $nodes;
     }
 }
